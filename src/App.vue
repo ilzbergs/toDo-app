6338efc7d5f119f3237396c8c62880e58da3e8eb
Toms Ilzbergs <ilzbergs@inbox.lv>
2023-04-14 10:39:26 +0300

    added add, delete and edit function

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,24 @@
 <template>
   <div>
-
+     <NewToDo />
   </div>
+ 
 </template>
 
+
 <script>
+
+import NewToDo from './components/NewToDo.vue';
+
 export default {
   name: 'App',
-
+  components: {
+    NewToDo
+  }
 }
 
 </script>
+
+
+
 
--- src/components/NewToDo.vue
+++ src/components/NewToDo.vue
@@ -1,12 +1,94 @@
 <template>
+    <h1>My To Do list!</h1>
     <div>
-
+        <p v-if="toDos.length === 0">Currently you have no any new ToDo </p>
+        <button v-if='editing' @click="toogler(false)">Cancel</button>
+        <button v-else='' @click="toogler(true)">New ToDo</button>
+    </div>
+    <div v-if="editing">
+        <label for="addToDo"></label>
+        <input type="text" id="addToDo" placeholder="Add To Do" v-model="toDo" @keyup.enter="addToDo">
+        <button @click="addToDo">ADD</button>
+    </div>
+    <div>
+        <ul v-for="(toDo, index) in toDos" :key="index">
+          <div v-if="editMode === index">
+              <input type="text" v-model="toDo.name" @keyup.enter="saveEdit" @blur="saveEdit" />
+              <button @click="cancelEdit">Cancel</button>
+            </div>
+            <div v-else>
+              <input type="checkbox" v-model="toDo.done" />
+              <span :class="{ done: toDo.done }">{{ toDo.name }}</span>
+              <button @click="editMode = index">Edit</button>
+              <button @click="deleteToDo(index)">Delete</button>
+            </div>
+        </ul>
     </div>
 </template>
+
 
 <script>
 export default {
 
-};
-</script>
+    name: 'AddToDo',
+    data() {
+        return {
+            toDo: '',
+            toDos: [],
+            editMode: null,
+            editing: false,
+        };
+    },
+    mounted() {
+        const storedTodos = localStorage.getItem('todos');
+        if (storedTodos) {
+            this.toDos = JSON.parse(storedTodos);
+        }
+    },
+    methods: {
+        saveToDos() {
+            localStorage.setItem('todos', JSON.stringify(this.toDos));
+        },
+        toogler(editing) {
+            this.editing = editing;
+            this.toDo = '';
+            this.visible = false;
+        },
 
+        addToDo() {
+            if (this.toDo.length === 0) {
+                return;
+            } if (this.editToDo != null) {
+                this.toDos[this.editToDo].name = this.toDo;
+                this.editToDo = null;
+            }
+            else {
+                this.toDos.push({
+                    name: this.toDo,
+                    done:false,
+                    id: Math.random(),
+                });
+                this.toDo = '';
+                this.saveToDos();
+            }
+        },
+        deleteToDo(index) {
+            this.toDos.splice(index, 1);
+            this.saveToDos();
+        },
+        enterEditMode(index) {
+            this.editMode = index;
+        },
+        saveEdit() {
+            this.editMode = null;
+            this.saveTodos();
+        },
+        cancelEdit() {
+            this.editMode = null;
+        },
+    },
+
+
+};
+
+</script>
